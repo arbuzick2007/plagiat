be74eb89ac45e27d66e29f90f5be665606ae6bea
arbuzick2007 <76817921+arbuzick2007@users.noreply.github.com>
2023-01-08 16:35:25 +0300

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,29 @@
+import sys
+
+def get_dist(str1, str2):
+    n = len(str1)
+    m = len(str2)
+    if m > n:
+        n, m = m, n
+        str1, str2 = str2, str1
+    dist = [[0] * (m + 1) for i in range(2)]
+    pos_calced = 0
+    for i in range(n):
+        for j in range(m):
+            dist[pos_calced ^ 1][j + 1] = max(dist[pos_calced][j + 1], dist[pos_calced][j] + int(str1[i] == str2[j]))
+        pos_calced ^= 1
+    return dist[pos_calced][m] / n
+
+f_input = open(sys.argv[1], "r")
+f_output = open(sys.argv[2], "w")
+
+for line in f_input.readlines():
+    files = line.split()
+    with open(files[0]) as f:
+        str1 = f.read()
+    with open(files[1]) as f:
+        str2 = f.read()
+    f_output.write(str(get_dist(str1, str2)) + '\n')
+
+f_input.close()
+f_output.close()
